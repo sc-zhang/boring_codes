0f4188c2516d0d7c42841dfaeb8f3f36bf2bccbe
sc-zhang <zsc-zhang@foxmail.com>
2023-05-22 09:50:14 +0800

    add hanoi c#

---
+++ cs/hanoi/Program.cs
@@ -0,0 +1,21 @@
+﻿class Hanoi
+{
+    static void Main(string[] args){
+        Console.WriteLine(args.Length);
+        if(args.Length != 1){
+            Console.WriteLine("usage: hanoi <number>");
+        }else{
+            int n = int.Parse(args[0]);
+            Move(n, 'A', 'B', 'C');
+        }
+    }
+    private static void Move(int number, char from , char mid, char to){
+        if(number==1){
+            Console.WriteLine("Move from {0} -> {1}", from, to);
+            return;
+        }
+        Move(number-1, from, to, mid);
+        Move(1, from ,mid, to);
+        Move(number-1, mid, from ,to);
+    }
+}
