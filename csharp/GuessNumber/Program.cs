ab6319aa5fd16c85d04e013d2a26d0cadc0cb3b3
sc-zhang <zsc-zhang@foxmail.com>
2023-05-31 09:42:06 +0800

    add guess number game

---
+++ csharp/GuessNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿class GuessNumber{
+  static void Main()
+  {
+    Random rand = new Random();
+    int ans = rand.Next(1, 1000);
+    int cnt = 0;
+    while(cnt<10){
+      Console.WriteLine("[{0}/10] Input a number less than 1000", cnt+1);
+      int input = Convert.ToInt32(Console.ReadLine());
+      if(input >= 1000 || input < 1){
+        Console.WriteLine("Input error, retry");
+        continue;
+      }
+      if(input > ans){
+        Console.WriteLine("Too big");
+      }else if(input < ans){
+        Console.WriteLine("Too small");
+      }else{
+        Console.WriteLine("You, win");
+        return;
+      }
+      cnt++;
+    }
+    Console.WriteLine("You lose, answer: {0}", ans);
+  }
+}
--- csharp/README.md
+++ csharp/README.md
@@ -6,8 +6,15 @@ dotnet publish -c Release --self-contained true
 # input a number, means plate counts
 ```
 
-### A/B Guess number
+### A/B Guess Number
 A/B Number Guess Game Guess a non-repeat 4-digit number, number with correct number and position will mark as A, number with correct number but wrong position as B.
 ```bash
 dotnet publish -c Release --self-contained true
 ```
+
+### Guess Number
+Guess number < 1000 in 10 times
+```
+dotnet publish -c Release --self-contained true
+```
+
